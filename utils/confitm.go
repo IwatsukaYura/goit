a2bcfb22affb172bec79be5e5cc5b0d543e1db20
岩塚由楽 <iwatsukayura@gmail.com>
2025-06-15 06:55:09 +0900

    feat: improve confirm commit dialog

--- utils/confitm.go
+++ utils/confitm.go
@@ -8,27 +8,32 @@ import (
 	"strings"
 )
 
-//ConfirmCommitMessage is 
+// ConfirmCommitMessage is
 func ConfirmCommitMessage(commitMessage string) bool {
-	fmt.Println("💬 Suggested commit message is below:")
-	fmt.Printf("\n%s\n", commitMessage)
+	fmt.Println("💬 Suggested commit message:")
+	fmt.Printf("%s\n\n", commitMessage)
 
-	fmt.Print("commit & push are you ready? (y/n): ")
-	reader := bufio.NewReader(os.Stdin)
-	isReady, _ := reader.ReadString('\n')
-	isReady = strings.TrimSpace(strings.ToLower(isReady))
-	if isReady == "n" {
-		fmt.Println("===================================")
-		fmt.Println("===================================")
-		fmt.Println(color.Red, "🚫 Commit canceled.", color.Reset)
+	
+	for {
+		fmt.Print("commit & push are you ready? (y/n): ")
+		reader := bufio.NewReader(os.Stdin)
+		isReady, _ := reader.ReadString('\n')
+		isReady = strings.TrimSpace(strings.ToLower(isReady))
+		
+		switch isReady {
+		case "n":
+			fmt.Println("===================================")
+			fmt.Println("===================================")
+			fmt.Println(color.Red, "🚫 Commit canceled.", color.Reset)
+			return false
+		case "y":
+			fmt.Println("===================================")
+			fmt.Println(color.Green, "🚀thank you for accepting commit message!", color.Reset)
+			return true
+		default:
+			fmt.Println(color.Red, "🚫 Please type 「y」 or 「n」", color.Reset)
+		}
 
-		return false
-	} else if isReady == "y" {
-		fmt.Println("===================================")
-		fmt.Println(color.Green, "🚀thank you for accepting commit message!", color.Reset)
-		return true
-	} else {
-		fmt.Println(color.Red, "🚫 Please type 「y」 or 「n」", color.Reset)
-		return false
 	}
+
 }
